ae5612261957544dc50f5b1247dbd62bf37fd196
Tomit Huynh <tomitnh@gmail.com>
2018-03-21 15:11:45 -0400

    Move the snake around. Spawn/Eat food

--- controller.js
+++ controller.js
@@ -2,23 +2,65 @@
  * Controller.js 
  *
  * Controls frame animation and drawing on the canvas
+ *
+ * 1) Create/spawn food 
+ * 2) Create snake's tail
+ * 3) Create losing condition
  */
 
-var fps = 5; // Standard frames per second
+var fps = 60; // Standard frames per second
 window.setInterval(animate, 1000/fps);
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-var s = new Snake();
-s.loc = new Point(20,20);
+var scl = 10;
+var p = new Point(scl,scl);
+var s = new Snake(p);
 s.xspeed = 1;
+
+var food = spawnFood();
+
+// Keyboard controller
+function steerSnake (e) {
+	switch (e.key) {
+		case 'ArrowLeft':
+			s.dir(-1,0);
+			break;
+		case 'ArrowRight':
+			s.dir(1,0);
+			break;
+		case 'ArrowUp':
+			s.dir(0,-1);
+			break;
+		case 'ArrowDown':
+			s.dir(0,1);
+			break;
+		default:
+			// nothing
+	}
+}
+document.addEventListener('keydown', steerSnake);
+
+function spawnFood () {
+	var f = new Point(Math.random() * canvas.width,
+	Math.random() * canvas.height);
+
+	return f;
+}
 
 function draw() {
 
 	ctx.fillStyle = 'grey';
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	if (s.eat(food))
+		food = spawnFood();
 	
+
+	ctx.fillStyle = 'red';
+	ctx.fillRect(food.x, food.y, scl, scl);
+
 	s.update();
 	s.show();
 }
--- snake.js
+++ snake.js
@@ -1,22 +1,79 @@
 class Snake {
-	constructor () {
-		this.loc = null; // x,y coord of the snake
+	constructor (point) {
 		this.xspeed = 0;
 		this.yspeed = 0;
+		this.body = [];
+		this.body[0] = point;
 	}
 
 	show () {
 		var canvas = document.getElementById('canvas');
 		var ctx = canvas.getContext('2d');
 		ctx.fillStyle = 'white';
-		ctx.fillRect(this.loc.x, 
-			this.loc.y, 
-			10, 
-			10);
+
+		for (var i = 0; i < this.body.length; i++) {
+			ctx.fillRect(this.body[i].x, 
+				this.body[i].y, 
+				10, 
+				10);			
+		}
+	}
+
+	dir (vx,vy) {
+		this.xspeed = vx;
+		this.yspeed = vy;
+	}
+
+	eat (food) {
+		var a = this.body[0].x - food.x;
+		var b = this.body[0].y - food.y;
+
+		var dist = Math.sqrt(a*a + b*b);
+
+		var eaten = dist < 5 ? true : false;
+
+		if (eaten) {
+			// grow the snake
+			var head = this.body[0];
+			var tail = new Point(0,0);
+
+			if (this.xspeed == 0) {
+				//snake was moving vertically
+				tail.x = head.x;
+				tail.y = head.y - this.yspeed;
+			} else {
+				tail.y = head.y;
+				tail.x = head.x - this.xspeed;
+			}
+
+			this.body[this.body.length] = tail;
+			
+		}
+
+		return eaten;
 	}
 
 	update () {
-		this.loc.x += this.xspeed;
-		this.loc.y += this.yspeed;
+		for (var i = this.body.length-1; i > 0; i--) {
+			var p = this.body[i-1];
+			this.body[i] = new Point(p.x,p.y);
+		}
+
+		var head = this.body[0];
+
+		head.x += this.xspeed;
+		head.y += this.yspeed;
+
+		// constrain x,y within the canvas border
+		var canvas = document.getElementById('canvas');
+		if (head.x < 0)
+			head.x = 0;
+		else if (head.x > canvas.width)
+			head.x = canvas.width;
+
+		if (head.y < 0)
+			head.y = 0;
+		else if (head.y > canvas.height)
+			head.y = canvas.height;
 	}
 }
